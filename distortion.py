5ec64e9400ad7e5a8e0b604e36201aaf3c46ce5d
edennisCS <muffin21@sky.com>
2024-01-11 20:39:48 +0000

    coords

--- distortion.py
+++ distortion.py
@@ -1,9 +1,28 @@
 from PIL import Image
 import numpy as np
+from scipy.spatial.transform import Rotation
+
 from src.cube import Cube
 from src.panel import Panel
 import matplotlib.pyplot as plt
 import scipy
+
+
+# the order is z, x, y
+def generate_cartesian_mesh_grid(x, y, z, angle, position):
+    # rotation on 3 axis
+    # rotation around z, define the rotation matrix
+    # rotation around x, "
+    # rotation around y, "
+    # translate position, move to the position
+    rotation_matrix = Rotation.from_euler('zxy', np.array([angle]))
+
+    # apply rotation
+    coordinates = np.vstack((x.flatten(), y.flatten(), z.flatten()))
+    rotated_coords = rotation_matrix.apply(coordinates)
+    cartesian_x, cartesian_y, cartesian_z = rotated_x + position[0], rotated_y + position[0], rotated_z + position[0]
+    return cartesian_x, cartesian_y, cartesian_z
+
 
 # Plot figure plt width 2000 height 1000
 
@@ -16,15 +35,8 @@ z_initial = 0
 
 x_untransformed, y_untransformed, z_untransformed = np.meshgrid(x_initial, y_initial, z_initial)
 
-# the order is z, x, y
-def generate_cartesian_mesh_grid(x, y, z, angle, position):
-    # rotation on 3 axis
-    # rotation around z, define the rotation matrix
-    # rotation around x, "
-    # rotation around y, "
-    # translate position, move to the position
-    rotation_matrix = Rotation.from_euler('zxy', np.array([angle]))
-
+cart_x, cart_y, cart_z = generate_cartesian_mesh_grid(x_initial, y_initial, z_initial, panel.angle, panel.position)
+print(cart_x, cart_y, cart_z)
 
 
 
