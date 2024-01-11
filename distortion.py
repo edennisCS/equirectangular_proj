bb391446c6957aa6db33a6bf2ff4ef8effe19629
edennisCS <muffin21@sky.com>
2024-01-11 22:27:29 +0000

    coords generation progress 2

--- distortion.py
+++ distortion.py
@@ -7,9 +7,11 @@ from src.panel import Panel
 import matplotlib.pyplot as plt
 import scipy
 
+
+
 def cartesian_to_equirectangular(x, y, z):
     lon = np.degrees(np.arctan2(x, y))
-    lat = np.degrees(np.arcsin(np.clip(z / np.sqrt(x**2 + y**2 + z**2), -1, 1)))
+    lat = np.degrees(np.arcsin(z / np.sqrt(x**2 + y**2 + z**2)))
     return lon, lat
 
 # the order is z, x, y
@@ -22,9 +24,9 @@ def generate_cartesian_mesh_grid(x, y, z, angle, position):
     rotation_matrix = Rotation.from_euler('zxy', np.array([angle]))
 
     mult = rotation_matrix.apply(np.array([x.ravel(), y.ravel(), z.ravel()]).transpose())
-    xrot = mult[:, 0].reshape(x.shape)
-    yrot = mult[:, 1].reshape(y.shape)
-    zrot = mult[:, 2].reshape(z.shape)
+    xrot = mult[:, 0]
+    yrot = mult[:, 1]
+    zrot = mult[:, 2]
 
     cartesian_x, cartesian_y, cartesian_z = xrot + position[0], yrot + position[1], zrot + position[2]
     return cartesian_x, cartesian_y, cartesian_z
@@ -35,9 +37,9 @@ def generate_cartesian_mesh_grid(x, y, z, angle, position):
 plt.figure(figsize=(2000, 1000))
 panel = Panel("images/example1.png", [0, 0, 0], [0, -1, 0], 2, 2)
 
-x_initial = np.linspace(0, panel.width, 100)
+x_initial = np.linspace(-panel.width/2, panel.width/2, 100)
 y_initial = np.linspace(0, 0, 100)
-z_initial = np.linspace(0, panel.height, 100)
+z_initial = np.linspace(-panel.width/2, panel.height/2, 100)
 
 x_untransformed, y_untransformed, z_untransformed = np.meshgrid(x_initial, y_initial, z_initial)
 
