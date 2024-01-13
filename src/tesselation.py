579d699934f768097aed2495e934a02cc65300a3
edennisCS <muffin21@sky.com>
2024-01-13 16:30:47 +0000

    added colour to tesselation

--- src/tesselation.py
+++ src/tesselation.py
@@ -1,10 +1,10 @@
 from src.panel import Panel
 
 class Tesselation:
-  def __init__(self):
+  def __init__(self, colours):
       for config in self.panel_configuration:
-        self.panels = map(self.createPanel, self.panel_configuration)
+        self.panels = map(self.createPanel, self.panel_configuration, colours)
 
-  def createPanel(_self, config):
-    return Panel(config['angle'], config['position'], config['width'], config['height'])
+  def createPanel(_self, config, colour):
+    return Panel(colour, config['angle'], config['position'], config['width'], config['height'])
 
