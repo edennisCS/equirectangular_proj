d65dee3c95dd2e3925a545c907e47268bebd3ffb
matharotheelf <thomaslawrence@thomass-Laptop.local>
2024-01-14 08:26:23 +0000

    Added code to cut coordinates into triangle is the tesseletion has a triangular mesh geometry

--- distortion.py
+++ distortion.py
@@ -1,8 +1,10 @@
 import numpy as np
 from scipy.spatial.transform import Rotation
 import matplotlib.pyplot as plt
+import matplotlib.path as mpath
 
 from src.cube import Cube
+from src.octohedron import Octohedron
 from src.panel import Panel
 
 # Function to convert Cartesian coordinates to equirectangular
@@ -41,19 +43,34 @@ def apply_translation_transformation(coordinate, translation):
     return coordinate + translation
 
 # Function to generate coordinates using panel
-def generate_coordinates(panel):
+def generate_coordinates(panel, face_geometry):
     """
 
     :param panel:
     :return: coordinates
     """
     x_initial = np.linspace(-panel.width / 2, panel.width / 2, 50)
-    y_initial = np.linspace(0, 0, 50)
-    z_initial = np.linspace(-panel.width / 2, panel.height / 2, 50)
+    z_initial = np.linspace(-panel.height / 2, panel.height / 2, 50)
 
-    x_grid, y_grid, z_grid = np.meshgrid(x_initial, y_initial, z_initial)
-    coordinates = np.column_stack((x_grid.ravel(), y_grid.ravel(), z_grid.ravel()))
+    x_grid, z_grid = np.meshgrid(x_initial, z_initial)
+    grid_array = np.column_stack((x_grid.ravel(), z_grid.ravel()))
+
+    cut_grid_array = cut_meshgrid_to_triangle(grid_array, panel) if face_geometry == "TRIANGLE" else grid_array 
+    coordinates = np.insert(cut_grid_array, 1, 0, axis=1)
     return coordinates
+
+# Function to cut a meshgrid to tesselation's face triangle geometry
+def cut_meshgrid_to_triangle(points, panel):
+   polygon_path = triangle_vertex_path(panel.width, panel.height)
+   points_mask = polygon_path.contains_points(points)
+
+   return points[points_mask]
+
+# Function to get the path of a polygon for a type of face geometryy
+def triangle_vertex_path(width, height):
+    polygon_vertices = np.array([[-width/2, -height/4], [width/2, -height/4], [0, height/2.85]])
+
+    return mpath.Path(polygon_vertices)
 
 # Function to apply transforms onto the coordinates
 def apply_transformations(coordinates, angle, translation):
@@ -71,7 +88,7 @@ def apply_transformations(coordinates, angle, translation):
     return equi_coordinates
 
 # The main logic of plotting the panels
-def plot_panels(panels):
+def plot_panels(panels, face_geometry):
     """
 
     :param panels:
@@ -79,7 +96,7 @@ def plot_panels(panels):
     plt.figure(figsize=(20, 10))
 
     for panel in panels:
-        coordinates = generate_coordinates(panel)
+        coordinates = generate_coordinates(panel, face_geometry)
         equi_coordinates = apply_transformations(coordinates, panel.angle, panel.position)
 
         lon = equi_coordinates[:, 0]
@@ -104,11 +121,10 @@ panels = [
     Panel("y", [0, 90, 0], [0, 0, -1], 2, 2)
 ]
 
-cube_instance = Cube(colours=['r', 'g', 'b', 'b', 'y', 'y'])
+cube_instance = Cube(colours=['r', 'g', 'b', 'b', 'y', 'y', 'r', 'r'])
 
 for panel in cube_instance.panels:
     print(f"Colour {panel.colour} Position: {panel.position}, Angle: {panel.angle}, Width: {panel.width}, Height: {panel.height}")
 
-
 # plot_panels(panels)
-plot_panels(cube_instance.panels)
+plot_panels(cube_instance.panels, cube_instance.face_geometry)
